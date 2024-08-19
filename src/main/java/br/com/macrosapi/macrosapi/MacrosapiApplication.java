ccae6c93f1b7e163d354df31f44c37d6059458ce
Caio Belchior <caio.belchior@hotmail.com>
2024-08-19 19:50:09 -0300

    projeto inicial

---
+++ src/main/java/br/com/macrosapi/macrosapi/MacrosapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.macrosapi.macrosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacrosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacrosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/macrosapi/macrosapi/MacrosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.macrosapi.macrosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacrosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
