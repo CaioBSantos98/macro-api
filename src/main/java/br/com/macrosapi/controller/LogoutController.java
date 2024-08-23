c93326ede00a40dbdf8a84df6f93b52f77dfb2bf
Caio Belchior <caio.belchior@hotmail.com>
2024-08-23 16:42:46 -0300

    add logout funcionality

---
+++ src/main/java/br/com/macrosapi/controller/LogoutController.java
@@ -0,0 +1,23 @@
+package br.com.macrosapi.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("custom-logout")
+public class LogoutController {
+
+    @PostMapping
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("JWT", null);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
+    }
+}
